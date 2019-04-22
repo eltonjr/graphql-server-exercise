d7669506b79bc464874680e356b6cbba7893cb58
Elton Hoffmann <elton.hoffmann@neoway.com.br>
2019-04-22 20:34:17 -0300

    Adding dummy data in dao
    Some data was added to the dao (db package) to simulate a database.
    Every response is static and no data is ever changed

--- db/driver.go
+++ db/driver.go
@@ -1,38 +1,56 @@
 package db
 
 import (
-	"errors"
-
 	"github.com/eltonjr/graphql-server-exercise/model"
 )
 
 type DriverDao struct {
+	dummyData []*model.Driver
 }
 
 func NewDriverDao() *DriverDao {
-	return &DriverDao{}
+	return &DriverDao{
+		dummyData: []*model.Driver{
+			&model.Driver{
+				ID:      "1",
+				Name:    "Kimi Raikkonen",
+				Country: "Finland",
+			},
+			&model.Driver{
+				ID:      "2",
+				Name:    "Romain Grosjean",
+				Country: "France",
+			},
+			&model.Driver{
+				ID:      "3",
+				Name:    "Nico Hulkenberg",
+				Country: "Germany",
+			},
+			&model.Driver{
+				ID:      "4",
+				Name:    "Daniel Riccardo",
+				Country: "Australia",
+			},
+		},
+	}
 }
 
 func (dd *DriverDao) GetDrivers() ([]*model.Driver, error) {
-	return nil, errors.New("TODO: method not implemented")
+	return dd.dummyData, nil
 }
 
 func (dd *DriverDao) GetSingleDriver(id string) (*model.Driver, error) {
-	return &model.Driver{
-		ID:      "1",
-		Name:    "Kimi Raikkonen",
-		Country: "Finland",
-	}, nil
+	return dd.dummyData[0], nil
 }
 
 func (dd *DriverDao) CreateDriver(*model.Driver) (*model.Driver, error) {
-	return nil, errors.New("TODO: method not implemented")
+	return dd.dummyData[1], nil
 }
 
 func (dd *DriverDao) UpdateDriver(*model.Driver) (*model.Driver, error) {
-	return nil, errors.New("TODO: method not implemented")
+	return dd.dummyData[2], nil
 }
 
 func (dd *DriverDao) DeleteDriver(id string) (*model.Driver, error) {
-	return nil, errors.New("TODO: method not implemented")
+	return dd.dummyData[3], nil
 }
